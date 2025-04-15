db8f6bd22e74e53eddcc30b28ba8f954e04a2216
sahil-singh1107 <sahilpython@gmail.com>
2025-04-15 20:58:37 +0530

    added prisma schema and lib for auth

---
+++ src/lib/cookie.ts
@@ -0,0 +1,35 @@
+import {cookies} from "next/headers";
+import {cache} from "react";
+import {validateSessionToken} from "@/lib/session";
+
+export async function setSessionCookie (token : string, expiresAt : Date) {
+    const cookieStore = await cookies();
+    cookieStore.set("session", token, {
+        httpOnly : true,
+        sameSite : "lax",
+        secure: process.env.NODE_ENV === "production",
+        expires: expiresAt,
+        path: "/"
+    })
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+    const cookieStore = await cookies();
+    cookieStore.set("session", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 0,
+        path: "/"
+    });
+}
+
+export const getCurrentSession = cache(async () => {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("session")?.value ?? null;
+    if (token === null) {
+        return { session: null, user: null };
+    }
+    const result = await validateSessionToken(token);
+    return result;
+});
